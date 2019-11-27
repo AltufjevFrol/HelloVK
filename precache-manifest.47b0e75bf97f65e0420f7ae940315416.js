8cb252525650ef5d2dbd09ef7316b4bc8f3c443b
Pilipenko Artem <artem.mihailovich@gmail.com>
2019-11-27 21:42:32 +0300

    Add files via upload

---
+++ precache-manifest.47b0e75bf97f65e0420f7ae940315416.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07036fca31487da10fddcd0b57a80710",
+    "url": "/HelloVK/index.html"
+  },
+  {
+    "revision": "d7ec94b23ea54c758051",
+    "url": "/HelloVK/static/css/main.175a3924.chunk.css"
+  },
+  {
+    "revision": "e071190be6c7c25e584c",
+    "url": "/HelloVK/static/js/2.010055af.chunk.js"
+  },
+  {
+    "revision": "d7ec94b23ea54c758051",
+    "url": "/HelloVK/static/js/main.1e539eab.chunk.js"
+  },
+  {
+    "revision": "c9fb529cad66324d7896",
+    "url": "/HelloVK/static/js/runtime-main.d09dd231.js"
+  }
+]);
